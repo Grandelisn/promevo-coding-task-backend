2f1136a539b7967a9f89c606972b7c0647e09e5e
Nate Grandelis <grandelisn@gmail.com>
2026-06-21 15:23:39 -0400

    project setup, initial commit

---
+++ src/test/java/com/example/promevocodingtaskbackend/PromevoCodingTaskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.promevocodingtaskbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromevoCodingTaskBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
